eabb59c882efa754281f9d76640cc04e104304ca
Saim20 <saimulislam2002@gmail.com>
2021-08-01 18:08:05 +0600

    Phew.....

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
-import 'package:readiew/pages/homeSetter.dart';
-import 'dart:math';
-
-import 'package:readiew/services/user.dart';
+import 'package:readiew/pages/subPages/HomeSub.dart';
 
 class RealHome extends StatefulWidget {
   const RealHome({
@@ -17,264 +11,235 @@ class RealHome extends StatefulWidget {
 }
 
 class _RealHomeState extends State<RealHome> {
-  bool locationEnabled = false;
-  bool permissionGranted = false;
-  bool rejected = false;
-  bool updateFlag = false;
-  LocationData? locationData;
-
-  double latMax = 0;
-  double latMin = 0;
-
-  double longMax = 0;
-  double longMin = 0;
-
-  calculateMinMax() {
-    latMax = (HomeSetterPage.mainUser!.lat ?? 0) + 0.1;
-    latMin = (HomeSetterPage.mainUser!.lat ?? 0) - 0.1;
-    longMax = (HomeSetterPage.mainUser!.long ?? 0) + 0.1;
-    longMin = (HomeSetterPage.mainUser!.long ?? 0) - 0.1;
-    // print(latMin.toString() +
-    //     ' ' +
-    //     latMax.toString() +
-    //     ' ' +
-    //     longMin.toString() +
-    //     ' ' +
-    //     longMax.toString());
-  }
-
-  getLocation() async {
-    print('Getting location...');
-
-    try {
-      Location location = new Location();
-
-      bool _serviceEnabled;
-      PermissionStatus _permissionGranted;
-
-      _serviceEnabled = await location.serviceEnabled();
-      locationEnabled = _serviceEnabled;
-      if (!_serviceEnabled) {
-        _serviceEnabled = await location.requestService();
-        if (!_serviceEnabled) {
-          setState(() {
-            print('Location service not enabled...');
-            locationEnabled = false;
-            rejected = true;
-          });
-          return;
-        }
-        print('Location service enabled...');
-        locationEnabled = true;
-      }
-
-      _permissionGranted = await location.hasPermission();
-      if (_permissionGranted == PermissionStatus.denied) {
-        _permissionGranted = await location.requestPermission();
-        if (!(_permissionGranted == PermissionStatus.granted ||
-            _permissionGranted == PermissionStatus.grantedLimited)) {
-          setState(() {
-            print('Location permission denied...');
-            permissionGranted = false;
-            rejected = true;
-          });
-          return;
-        }
-        print('Location permission granted...');
-        permissionGranted = true;
-      }
-
-      locationData = await location.getLocation();
-      //Calculate minimum and maximum for other distances
-      calculateMinMax();
-      await uploadLocation(locationData!);
-      updateFlag = true;
-      setState(() {});
-      print(locationData);
-    } catch (e) {
-      print(e);
+  int selectedIndex = 0;
+  pageSetter(int index) {
+    switch (index) {
+      case 0:
+        return HomeSubPage();
+      case 1:
+        return ColoredBox(color: Colors.green);
+      case 2:
+        return ColoredBox(color: Colors.red);
+      default:
+        return HomeSubPage();
     }
   }
 
-  uploadLocation(LocationData locationData) {
-    print('Uploading location...');
-    try {
-      FirebaseFirestore.instance
-          .collection('users')
-          .doc(FirebaseAuth.instance.currentUser!.uid)
-          .update({
-        'lat': locationData.latitude,
-        'long': locationData.longitude,
-      });
-      print('Location uploaded...');
-    } catch (e) {
-      print('Uploading failed...');
-      print(e);
-    }
-  }
-
-  double calculateDistance(lat1, lon1, lat2, lon2) {
-    var p = 0.017453292519943295;
-    var c = cos;
-    var a = 0.5 -
-        c((lat2 - lat1) * p) / 2 +
-        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
-    return 12742 * asin(sqrt(a));
+  setSelectedIndex(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!rejected && !updateFlag) {
-      getLocation();
-    }
-    if (updateFlag) {
-      updateFlag = false;
-    }
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cadets Nearby'),
+      body: Stack(
+        children: [
+          pageSetter(selectedIndex),
+          CustomBottomNavigationBar(setIndex: setSelectedIndex),
+        ],
       ),
-      body: SafeArea(
-        child: Container(
-          child: ListView(
-            children: [
-              Column(
-                children: [
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Text(
-                    'Raw Data',
-                    style:
-                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-                  ),
-                  Text(locationData == null
-                      ? 'Loading'
-                      : 'Latitude: ' + locationData!.latitude.toString()),
-                  Text(locationData == null
-                      ? 'Loading'
-                      : 'Longtitude: ' + locationData!.longitude.toString()),
-                  ElevatedButton(
-                    child: Text('Sign Out'),
-                    onPressed: () {
-                      FirebaseAuth.instance.signOut();
-                    },
-                  ),
-                ],
-              ),
-              if (locationData != null)
-                FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                  future: FirebaseFirestore.instance
-                      .collection('users')
-                      .where('plocation', isEqualTo: true)
-                      .where('long',
-                          isLessThan: longMax, isGreaterThan: longMin)
-                      .get(),
-                  builder: (context, snapshots) {
-                    // TODO (( unimportant )) show NOONE NEAR for nothing found
-                    if (snapshots.hasData) {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: snapshots.data!.docs.length == 0
-                            ? [Text('No one nearby')]
-                            : snapshots.data!.docs.map(
-                                (u) {
-                                  // Make a user object
-                                  AppUser e = AppUser(
-                                    cName: u.data()['cname'],
-                                    cNumber: int.parse(u.data()['cnumber']),
-                                    fullName: u.data()['fullname'],
-                                    college: u.data()['college'],
-                                    email: u.data()['email'],
-                                    intake: int.parse(u.data()['intake']),
-                                    lat: u.data()['lat'],
-                                    long: u.data()['long'],
-                                    pAlways: u.data()['palways'],
-                                    pLocation: u.data()['plocation'],
-                                    pMaps: u.data()['pmaps'],
-                                    pPhone: u.data()['pphone'],
-                                    phone: u.data()['phone'],
-                                  );
+    );
+  }
+}
 
-                                  if (e.equals(HomeSetterPage.mainUser!) &&
-                                      snapshots.data!.docs.length == 1) {
-                                    return Text('No one nearby');
-                                  } else if (e
-                                      .equals(HomeSetterPage.mainUser!)) {
-                                    return SizedBox();
-                                  } else if ((e.lat ?? 0) > latMax ||
-                                      (e.lat ?? 0) < latMin) {
-                                    return SizedBox();
-                                  }
-                                  // Get distance in metres
-                                  var distanceD = calculateDistance(
-                                          locationData!.latitude,
-                                          locationData!.longitude,
-                                          e.lat,
-                                          e.long) *
-                                      1000;
-                                  double distance = 0;
-                                  int distanceInt = 0;
-                                  bool isKm = false;
+class CustomBottomNavigationBar extends StatefulWidget {
+  CustomBottomNavigationBar({
+    Key? key,
+    required this.setIndex,
+  }) : super(key: key);
 
-                                  if (distanceD < 10) {
-                                    distanceInt = distanceD.toInt();
-                                  } else {
-                                    distance = distanceD.roundToDouble() -
-                                        distanceD.roundToDouble() % 10;
-                                    if (distance > 1000) {
-                                      distance /= 1000;
-                                      distance = double.parse(
-                                          distance.toStringAsFixed(2));
-                                      isKm = true;
-                                    }
-                                    if (!isKm) {
-                                      distanceInt = distance.round();
-                                    }
-                                  }
+  final Function setIndex;
 
-                                  return Container(
-                                    margin: EdgeInsets.fromLTRB(
-                                        10.0, 5.0, 10.0, 5.0),
-                                    child: Card(
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(10.0),
-                                        child: Column(
-                                          children: [
-                                            Text(
-                                              'Name: ' + e.fullName,
-                                            ),
-                                            Text(
-                                              'Latitude: ' + e.lat.toString(),
-                                            ),
-                                            Text(
-                                              'Longtitude: ' +
-                                                  e.long.toString(),
-                                            ),
-                                            Text(
-                                              (isKm
-                                                      ? distance.toString()
-                                                      : distanceInt
-                                                          .toString()) +
-                                                  (isKm ? 'km' : 'm'),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ).toList(),
-                      );
-                    }
-                    return SizedBox();
-                  },
-                ),
-            ],
-          ),
+  @override
+  _CustomBottomNavigationBarState createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  var pages = [true, false, false];
+
+  setSelected(int index) {
+    setState(() {
+      widget.setIndex(index);
+      switch (index) {
+        case 0:
+          pages = [true, false, false];
+          break;
+        case 1:
+          pages = [false, true, false];
+          break;
+        case 2:
+          pages = [false, false, true];
+          break;
+        default:
+          pages = [true, false, false];
+          break;
+      }
+    });
+  }
+
+  buildNavItem(IconData icon, bool active, int selected) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(30.0),
+      onTap: () {
+        setSelected(selected);
+      },
+      child: CircleAvatar(
+        radius: 30,
+        backgroundColor: Theme.of(context).accentColor,
+        child: CircleAvatar(
+          radius: 20,
+          backgroundColor:
+              active ? Colors.white.withOpacity(0.5) : Colors.white,
+          child: Icon(icon),
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 0,
+          child: ClipPath(
+            clipper: NavBarClipper(),
+            child: Container(
+              height: 60,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Theme.of(context).accentColor,
+                    Theme.of(context).primaryColor,
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 45,
+          width: MediaQuery.of(context).size.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              buildNavItem(Icons.home, pages[0], 0),
+              SizedBox(),
+              buildNavItem(Icons.search, pages[1], 1),
+              SizedBox(),
+              buildNavItem(Icons.account_circle, pages[2], 2),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 10,
+          width: MediaQuery.of(context).size.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                'Home',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                width: 1,
+              ),
+              Text(
+                'Search',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                width: 1,
+              ),
+              Text(
+                'Account',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class NavBarClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    Path path = Path();
+    var sw = size.width;
+    var sh = size.height;
+    path.cubicTo(
+      sw / 12,
+      0,
+      sw / 12,
+      2 * sh / 5,
+      2 * sw / 12,
+      2 * sh / 5,
+    );
+    path.cubicTo(
+      3 * sw / 12,
+      2 * sh / 5,
+      3 * sw / 12,
+      0,
+      4 * sw / 12,
+      0,
+    );
+    path.cubicTo(
+      5 * sw / 12,
+      0,
+      5 * sw / 12,
+      2 * sh / 5,
+      6 * sw / 12,
+      2 * sh / 5,
+    );
+    path.cubicTo(
+      7 * sw / 12,
+      2 * sh / 5,
+      7 * sw / 12,
+      0,
+      8 * sw / 12,
+      0,
+    );
+    path.cubicTo(
+      9 * sw / 12,
+      0,
+      9 * sw / 12,
+      2 * sh / 5,
+      10 * sw / 12,
+      2 * sh / 5,
+    );
+    path.cubicTo(
+      11 * sw / 12,
+      2 * sh / 5,
+      11 * sw / 12,
+      0,
+      sw,
+      0,
+    );
+    path.lineTo(sw, sh);
+    path.lineTo(0, sh);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper oldClipper) {
+    return false;
   }
 }
---
+++ lib/pages/subPages/HomeSub.dart
@@ -0,0 +1,265 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:readiew/pages/homeSetter.dart';
+import 'package:readiew/services/user.dart';
+
+class HomeSubPage extends StatefulWidget {
+  HomeSubPage({Key? key}) : super(key: key);
+
+  @override
+  _HomeSubPageState createState() => _HomeSubPageState();
+}
+
+class _HomeSubPageState extends State<HomeSubPage> {
+  bool locationEnabled = false;
+  bool permissionGranted = false;
+  bool rejected = false;
+  bool updateFlag = false;
+  LocationData? locationData;
+
+  double latMax = 0;
+  double latMin = 0;
+
+  double longMax = 0;
+  double longMin = 0;
+
+  calculateMinMax() {
+    latMax = (HomeSetterPage.mainUser!.lat ?? 0) + 0.1;
+    latMin = (HomeSetterPage.mainUser!.lat ?? 0) - 0.1;
+    longMax = (HomeSetterPage.mainUser!.long ?? 0) + 0.1;
+    longMin = (HomeSetterPage.mainUser!.long ?? 0) - 0.1;
+    // print(latMin.toString() +
+    //     ' ' +
+    //     latMax.toString() +
+    //     ' ' +
+    //     longMin.toString() +
+    //     ' ' +
+    //     longMax.toString());
+  }
+
+  getLocation() async {
+    print('Getting location...');
+
+    try {
+      Location location = new Location();
+
+      bool _serviceEnabled;
+      PermissionStatus _permissionGranted;
+
+      _serviceEnabled = await location.serviceEnabled();
+      locationEnabled = _serviceEnabled;
+      if (!_serviceEnabled) {
+        _serviceEnabled = await location.requestService();
+        if (!_serviceEnabled) {
+          setState(() {
+            print('Location service not enabled...');
+            locationEnabled = false;
+            rejected = true;
+          });
+          return;
+        }
+        print('Location service enabled...');
+        locationEnabled = true;
+      }
+
+      _permissionGranted = await location.hasPermission();
+      if (_permissionGranted == PermissionStatus.denied) {
+        _permissionGranted = await location.requestPermission();
+        if (!(_permissionGranted == PermissionStatus.granted ||
+            _permissionGranted == PermissionStatus.grantedLimited)) {
+          setState(() {
+            print('Location permission denied...');
+            permissionGranted = false;
+            rejected = true;
+          });
+          return;
+        }
+        print('Location permission granted...');
+        permissionGranted = true;
+      }
+
+      locationData = await location.getLocation();
+      //Calculate minimum and maximum for other distances
+      calculateMinMax();
+      await uploadLocation(locationData!);
+      updateFlag = true;
+      setState(() {});
+      print(locationData);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  uploadLocation(LocationData locationData) {
+    print('Uploading location...');
+    try {
+      FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .update({
+        'lat': locationData.latitude,
+        'long': locationData.longitude,
+      });
+      print('Location uploaded...');
+    } catch (e) {
+      print('Uploading failed...');
+      print(e);
+    }
+  }
+
+  double calculateDistance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;
+    var c = cos;
+    var a = 0.5 -
+        c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
+    return 12742 * asin(sqrt(a));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!rejected && !updateFlag) {
+      getLocation();
+    }
+    if (updateFlag) {
+      updateFlag = false;
+    }
+    return Container(
+      child: ListView(
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'Raw Data',
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+              Text(locationData == null
+                  ? 'Loading'
+                  : 'Latitude: ' + locationData!.latitude.toString()),
+              Text(locationData == null
+                  ? 'Loading'
+                  : 'Longtitude: ' + locationData!.longitude.toString()),
+              ElevatedButton(
+                child: Text('Sign Out'),
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+              ),
+            ],
+          ),
+          if (locationData != null)
+            FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .where('plocation', isEqualTo: true)
+                  .where('long', isLessThan: longMax, isGreaterThan: longMin)
+                  .get(),
+              builder: (context, snapshots) {
+                // TODO (( unimportant )) show NOONE NEAR for nothing found
+                if (snapshots.hasData) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: snapshots.data!.docs.length == 0
+                        ? [Text('No one nearby')]
+                        : snapshots.data!.docs.map(
+                            (u) {
+                              // Make a user object
+                              AppUser e = AppUser(
+                                cName: u.data()['cname'],
+                                cNumber: int.parse(u.data()['cnumber']),
+                                fullName: u.data()['fullname'],
+                                college: u.data()['college'],
+                                email: u.data()['email'],
+                                intake: int.parse(u.data()['intake']),
+                                lat: u.data()['lat'],
+                                long: u.data()['long'],
+                                pAlways: u.data()['palways'],
+                                pLocation: u.data()['plocation'],
+                                pMaps: u.data()['pmaps'],
+                                pPhone: u.data()['pphone'],
+                                phone: u.data()['phone'],
+                              );
+
+                              if (e.equals(HomeSetterPage.mainUser!) &&
+                                  snapshots.data!.docs.length == 1) {
+                                return Text('No one nearby');
+                              } else if (e.equals(HomeSetterPage.mainUser!)) {
+                                return SizedBox();
+                              } else if ((e.lat ?? 0) > latMax ||
+                                  (e.lat ?? 0) < latMin) {
+                                return SizedBox();
+                              }
+                              // Get distance in metres
+                              var distanceD = calculateDistance(
+                                      locationData!.latitude,
+                                      locationData!.longitude,
+                                      e.lat,
+                                      e.long) *
+                                  1000;
+                              double distance = 0;
+                              int distanceInt = 0;
+                              bool isKm = false;
+
+                              if (distanceD < 10) {
+                                distanceInt = distanceD.toInt();
+                              } else {
+                                distance = distanceD.roundToDouble() -
+                                    distanceD.roundToDouble() % 10;
+                                if (distance > 1000) {
+                                  distance /= 1000;
+                                  distance =
+                                      double.parse(distance.toStringAsFixed(2));
+                                  isKm = true;
+                                }
+                                if (!isKm) {
+                                  distanceInt = distance.round();
+                                }
+                              }
+
+                              return Container(
+                                margin:
+                                    EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+                                child: Card(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          'Name: ' + e.fullName,
+                                        ),
+                                        Text(
+                                          'Latitude: ' + e.lat.toString(),
+                                        ),
+                                        Text(
+                                          'Longtitude: ' + e.long.toString(),
+                                        ),
+                                        Text(
+                                          (isKm
+                                                  ? distance.toString()
+                                                  : distanceInt.toString()) +
+                                              (isKm ? 'km' : 'm'),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ).toList(),
+                  );
+                }
+                return SizedBox();
+              },
+            ),
+        ],
+      ),
+    );
+  }
+}
