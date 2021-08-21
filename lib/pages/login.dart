2e5d7f9065ecf81777992a12035834757dfb619e
Saim20 <saimulislam2002@gmail.com>
2021-08-21 18:10:29 +0600

    Things I forgot to do

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:cadets_nearby/data/appData.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cadets_nearby/pages/homeSetter.dart';
