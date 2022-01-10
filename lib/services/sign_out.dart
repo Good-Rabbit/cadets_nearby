4ebb76c6cb404ad147003e7b2d121c7c8f579910
Saim20 <saimulislam2002@gmail.com>
2022-01-10 16:52:53 +0600

    Changes to Background service

--- lib/services/sign_out.dart
+++ lib/services/sign_out.dart
@@ -1,5 +1,5 @@
 import 'package:cadets_nearby/pages/home_setter.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
+// import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -9,7 +9,7 @@ void signOut() async {
   HomeSetterPage.auth.signOut();
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   prefs.clear();
-  FlutterBackgroundService().sendData({'action': 'stopService'});
-  FlutterBackgroundService().stopBackgroundService();
-  FlutterBackgroundService().dispose();
+  // FlutterBackgroundService().sendData({'action': 'stopService'});
+  // FlutterBackgroundService().stopBackgroundService();
+  // FlutterBackgroundService().dispose();
 }
