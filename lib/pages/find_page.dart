712acec5be152bd843f8e1138a5621e61e6c31de
Saim20 <saimulislam2002@gmail.com>
2022-02-18 15:51:44 +0600

    Fixes

--- lib/pages/find_page.dart
+++ lib/pages/find_page.dart
@@ -98,55 +98,57 @@ class _FindPageState extends State<FindPage> {
                   if (snapshot.data!.docs.isEmpty) {
                     return Expanded(child: Center(child: noOneFound(context)));
                   } else {
-                    return Expanded(SingleChildScrollView(
-                      child: Column(
-                        children: snapshot.data!.docs.map((u) {
-                          final AppUser e = AppUser(
-                            id: u.data()['id'] as String,
-                            cName: u.data()['cname'] as String,
-                            cNumber: int.parse(u.data()['cnumber'] as String),
-                            fullName: u.data()['fullname'] as String,
-                            college: u.data()['college'] as String,
-                            email: u.data()['email'] as String,
-                            intake: int.parse(u.data()['intake'] as String),
-                            lat: u.data()['lat'] as double,
-                            long: u.data()['long'] as double,
-                            timeStamp: DateTime.parse(
-                                u.data()['lastonline'] as String),
-                            premiumTo: u.data()['premiumto'] == null
-                                ? DateTime.now()
-                                : DateTime.parse(
-                                    u.data()['premiumto'] as String),
-                            photoUrl: u.data()['photourl'] as String,
-                            pAlways: u.data()['palways'] as bool,
-                            pLocation: u.data()['plocation'] as bool,
-                            pMaps: u.data()['pmaps'] as bool,
-                            pPhone: u.data()['pphone'] as bool,
-                            phone: u.data()['phone'] as String,
-                            premium: u.data()['premium'] as bool,
-                            verified: u.data()['verified'] as String,
-                            fbUrl: u.data()['fburl'] as String,
-                            instaUrl: u.data()['instaurl'] as String,
-                            celeb: u.data()['celeb'] as bool,
-                            treatHead: u.data()['treathead'] as bool,
-                            treatHunter: u.data()['treathunter'] as bool,
-                            designation: u.data()['designation'] as String,
-                            profession: u.data()['profession'] as String,
-                            manualDp: u.data()['manualdp'] as bool,
-                            treatCount: u.data()['treatcount'] as int,
-                            sector: u.data()['sector'] as int,
-                            address: u.data()['address'] as String,
-                            contact: u.data()['contact'] as bool,
-                            coupons: u.data()['coupons'] as int,
-                          );
-                          return Container(
-                            margin:
-                                const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-                            child: ContactCard(e: e),
-                          );
-                        }).toList(),
+                    return Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: snapshot.data!.docs.map((u) {
+                            final AppUser e = AppUser(
+                              id: u.data()['id'] as String,
+                              cName: u.data()['cname'] as String,
+                              cNumber: int.parse(u.data()['cnumber'] as String),
+                              fullName: u.data()['fullname'] as String,
+                              college: u.data()['college'] as String,
+                              email: u.data()['email'] as String,
+                              intake: int.parse(u.data()['intake'] as String),
+                              lat: u.data()['lat'] as double,
+                              long: u.data()['long'] as double,
+                              timeStamp: DateTime.parse(
+                                  u.data()['lastonline'] as String),
+                              premiumTo: u.data()['premiumto'] == null
+                                  ? DateTime.now()
+                                  : DateTime.parse(
+                                      u.data()['premiumto'] as String),
+                              photoUrl: u.data()['photourl'] as String,
+                              pAlways: u.data()['palways'] as bool,
+                              pLocation: u.data()['plocation'] as bool,
+                              pMaps: u.data()['pmaps'] as bool,
+                              pPhone: u.data()['pphone'] as bool,
+                              phone: u.data()['phone'] as String,
+                              premium: u.data()['premium'] as bool,
+                              verified: u.data()['verified'] as String,
+                              fbUrl: u.data()['fburl'] as String,
+                              instaUrl: u.data()['instaurl'] as String,
+                              celeb: u.data()['celeb'] as bool,
+                              treatHead: u.data()['treathead'] as bool,
+                              treatHunter: u.data()['treathunter'] as bool,
+                              designation: u.data()['designation'] as String,
+                              profession: u.data()['profession'] as String,
+                              manualDp: u.data()['manualdp'] as bool,
+                              treatCount: u.data()['treatcount'] as int,
+                              sector: u.data()['sector'] as int,
+                              address: u.data()['address'] as String,
+                              contact: u.data()['contact'] as bool,
+                              coupons: u.data()['coupons'] as int,
+                            );
+                            return Container(
+                              margin: const EdgeInsets.fromLTRB(
+                                  10.0, 5.0, 10.0, 5.0),
+                              child: ContactCard(e: e),
+                            );
+                          }).toList(),
+                        ),
                       ),
-                    ),);
+                    );
                   }
                 }
                 return Container();
